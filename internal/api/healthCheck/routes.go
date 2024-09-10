7a73b033eb6af602d7cd35d10fee10c55db0a73d
milkymilky0116 <sjlee990129@gmail.com>
2024-09-10 23:51:00 +0900

    chore: refine health check (#5)

--- internal/api/healthCheck/routes.go
+++ internal/api/healthCheck/routes.go
@@ -4,7 +4,10 @@ import (
 	"context"
 	"net/http"
 
+	"github.com/milkymilky0116/goorm-be-1/internal/api/middleware"
+	"github.com/milkymilky0116/goorm-be-1/internal/util"
 	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
 )
 
 var tracer = otel.Tracer("health_check")
@@ -13,7 +16,11 @@ type HealthCheckController struct{}
 
 func (h *HealthCheckController) HealthCheck(w http.ResponseWriter, r *http.Request) {
 	_, span := tracer.Start(context.Background(), "HealthCheck")
+	requestID := util.GetRequestID(w, r)
+	spanID := span.SpanContext().SpanID().String()
+	span.SetAttributes(attribute.String(middleware.REQUEST_ID, *requestID))
 	defer span.End()
+	util.LogInfo(span, *requestID, spanID, "Health Check Complete")
 	w.WriteHeader(http.StatusOK)
 }
 
